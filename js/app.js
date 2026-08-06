ec5a3bf0f3e8c1c8e6d07fd04483cd4b8e544033
lemonroti <132274062+lemonroti@users.noreply.github.com>
2026-08-06 21:20:02 +0800

    feat: mount prototype application

---
+++ js/app.js
@@ -0,0 +1,20 @@
+import { router } from './router.js';
+import { SiteHeader, SiteFooter, BackToTop } from './components.js';
+
+const App = {
+  components: { SiteHeader, SiteFooter, BackToTop },
+  mounted() { this.$nextTick(() => window.lucide?.createIcons()); },
+  updated() { this.$nextTick(() => window.lucide?.createIcons()); },
+  template: `
+    <div class="min-h-screen bg-cream text-ink">
+      <SiteHeader />
+      <router-view />
+      <SiteFooter />
+      <BackToTop />
+    </div>
+  `
+};
+
+const app = Vue.createApp(App);
+app.use(router);
+app.mount('#app');
